d0e6ecf9d89bdce28816e409f8246de39548c36a
Neil Poulin <neil.j.poulin@gmail.com>
2018-11-10 23:36:31 -0700

    upgrade to babel 7

--- dist/index.js
+++ dist/index.js
@@ -1,81 +1,79 @@
-'use strict';
+const {
+  makeClassNameFromPath
+} = require("./utils");
 
-var _require = require("./utils"),
-    makeClassNameFromPath = _require.makeClassNameFromPath;
-
-var defaultIgnoredElements = ['React.Fragment', 'Fragment'];
+const defaultIgnoredElements = ["React.Fragment", "Fragment"];
 
 function doesElementNameMatch(elementNameNode, elements) {
-    var elementIdentifierParts = [];
+  const elementIdentifierParts = [];
+  let currentNode = elementNameNode;
 
-    var currentNode = elementNameNode;
-    while (currentNode.type === 'JSXMemberExpression') {
-        elementIdentifierParts.unshift(currentNode.property.name);
-        currentNode = currentNode.object;
-    }
-    elementIdentifierParts.unshift(currentNode.name);
+  while (currentNode.type === "JSXMemberExpression") {
+    elementIdentifierParts.unshift(currentNode.property.name);
+    currentNode = currentNode.object;
+  }
 
-    var elementName = elementIdentifierParts.join('.');
-    return elements.indexOf(elementName) !== -1;
+  elementIdentifierParts.unshift(currentNode.name);
+  const elementName = elementIdentifierParts.join(".");
+  return elements.indexOf(elementName) !== -1;
 }
 
-module.exports = function EncapsulateJsx(_ref) {
-    var t = _ref.types;
+module.exports = function EncapsulateJsx({
+  types: t
+}) {
+  return {
+    visitor: {
+      Program: function transform(path, state) {
+        const fileComments = path.parent.comments;
 
-    return {
-        visitor: {
-            Program: function transform(path, state) {
-                var fileComments = path.parent.comments;
-                if (fileComments && fileComments.length) {
-                    if (fileComments.filter(function (comment) {
-                        return comment.value.indexOf('disable-encapsulation') !== -1;
-                    }).length > 0) {
-                        state.disableEncapsulation = true;
-                    }
-                }
-            },
-
-            JSXOpeningElement: function transform(path, state) {
-                if (path.node.encapsulatedAlready || state.disableEncapsulation === true) {
-                    return;
-                }
-
-                var ignoredElements = state.opts.ignoredElements || defaultIgnoredElements;
-                if (doesElementNameMatch(path.node.name, ignoredElements)) {
-                    return;
-                }
-
-                var className = makeClassNameFromPath(state.file.log.filename);
-
-                var classnameAttributes = path.node.attributes.filter(function (a) {
-                    return t.isJSXAttribute(a) && t.isJSXIdentifier(a.name, { name: 'className' });
-                });
-                if (!classnameAttributes.length) {
-                    var node = t.jSXOpeningElement(path.node.name, path.node.attributes.concat([t.jSXAttribute(t.jSXIdentifier('className'), t.stringLiteral(className))]), path.node.selfClosing);
-                    node.encapsulatedAlready = true;
-                    path.replaceWith(node);
-                } else {
-                    classnameAttributes.forEach(function (attr) {
-                        if (t.isStringLiteral(attr.value)) {
-                            var _node = t.jSXOpeningElement(path.node.name, path.node.attributes.map(function (curAttr) {
-                                if (attr !== curAttr) return curAttr;
-                                return t.jSXAttribute(t.jSXIdentifier('className'), t.stringLiteral(attr.value.value + ' ' + className));
-                            }), path.node.selfClosing);
-                            _node.encapsulatedAlready = true;
-                            path.replaceWith(_node);
-                        } else if (t.isJSXExpressionContainer(attr.value)) {
-                            var _node2 = t.jSXOpeningElement(path.node.name, path.node.attributes.map(function (curAttr) {
-                                if (attr !== curAttr) return curAttr;
-                                return t.jSXAttribute(t.jSXIdentifier('className'), t.jSXExpressionContainer(t.binaryExpression('+', attr.value.expression, t.stringLiteral(' ' + className))));
-                            }), path.node.selfClosing);
-                            _node2.encapsulatedAlready = true;
-                            path.replaceWith(_node2);
-                        } else {
-                            throw new Error('Babel Plugin Encapsulate JSX: unknown attribute type');
-                        }
-                    });
-                }
-            }
+        if (fileComments && fileComments.length) {
+          if (fileComments.filter(comment => comment.value.indexOf("disable-encapsulation") !== -1).length > 0) {
+            state.disableEncapsulation = true;
+          }
         }
-    };
+      },
+      JSXOpeningElement: function transform(path, state) {
+        if (path.node.encapsulatedAlready || state.disableEncapsulation === true) {
+          return;
+        }
+
+        const ignoredElements = state.opts.ignoredElements || defaultIgnoredElements;
+
+        if (doesElementNameMatch(path.node.name, ignoredElements)) {
+          return;
+        }
+
+        const className = makeClassNameFromPath(state.file.log.filename);
+        const classnameAttributes = path.node.attributes.filter(a => t.isJSXAttribute(a) && t.isJSXIdentifier(a.name, {
+          name: "className"
+        }));
+
+        if (!classnameAttributes.length) {
+          const node = t.jSXOpeningElement(path.node.name, path.node.attributes.concat([t.jSXAttribute(t.jSXIdentifier("className"), t.stringLiteral(className))]), path.node.selfClosing);
+          node.encapsulatedAlready = true;
+          path.replaceWith(node);
+        } else {
+          classnameAttributes.forEach(attr => {
+            if (t.isStringLiteral(attr.value)) {
+              const node = t.jSXOpeningElement(path.node.name, path.node.attributes.map(curAttr => {
+                if (attr !== curAttr) return curAttr;
+                return t.jSXAttribute(t.jSXIdentifier("className"), t.stringLiteral(`${attr.value.value} ${className}`));
+              }), path.node.selfClosing);
+              node.encapsulatedAlready = true;
+              path.replaceWith(node);
+            } else if (t.isJSXExpressionContainer(attr.value)) {
+              const node = t.jSXOpeningElement(path.node.name, path.node.attributes.map(curAttr => {
+                if (attr !== curAttr) return curAttr;
+                return t.jSXAttribute(t.jSXIdentifier("className"), t.jSXExpressionContainer(t.binaryExpression("+", attr.value.expression, t.stringLiteral(` ${className}`))));
+              }), path.node.selfClosing);
+              node.encapsulatedAlready = true;
+              path.replaceWith(node);
+            } else {
+              throw new Error("Babel Plugin Encapsulate JSX: unknown attribute type");
+            }
+          });
+        }
+      }
+    }
+  };
 };
--- src/index.js
+++ src/index.js
@@ -1,100 +1,115 @@
-const {makeClassNameFromPath} = require("./utils");
+const { makeClassNameFromPath } = require("./utils");
 
-const defaultIgnoredElements = ['React.Fragment', 'Fragment'];
+const defaultIgnoredElements = ["React.Fragment", "Fragment"];
 
 function doesElementNameMatch(elementNameNode, elements) {
-    const elementIdentifierParts = [];
+  const elementIdentifierParts = [];
 
-    let currentNode = elementNameNode;
-    while (currentNode.type === 'JSXMemberExpression') {
-        elementIdentifierParts.unshift(currentNode.property.name);
-        currentNode = currentNode.object;
-    }
-    elementIdentifierParts.unshift(currentNode.name);
+  let currentNode = elementNameNode;
+  while (currentNode.type === "JSXMemberExpression") {
+    elementIdentifierParts.unshift(currentNode.property.name);
+    currentNode = currentNode.object;
+  }
+  elementIdentifierParts.unshift(currentNode.name);
 
-    const elementName = elementIdentifierParts.join('.');
-    return elements.indexOf(elementName) !== -1;
+  const elementName = elementIdentifierParts.join(".");
+  return elements.indexOf(elementName) !== -1;
 }
 
-module.exports = function EncapsulateJsx({types: t}) {
-    return {
-        visitor: {
-            Program: function transform(path, state) {
-                const fileComments = path.parent.comments;
-                if (fileComments && fileComments.length) {
-                    if (fileComments.filter(comment => comment.value.indexOf('disable-encapsulation') !== -1).length > 0) {
-                        state.disableEncapsulation = true;
-                    }
-                }
-            },
+module.exports = function EncapsulateJsx({ types: t }) {
+  return {
+    visitor: {
+      Program: function transform(path, state) {
+        const fileComments = path.parent.comments;
+        if (fileComments && fileComments.length) {
+          if (
+            fileComments.filter(
+              comment => comment.value.indexOf("disable-encapsulation") !== -1
+            ).length > 0
+          ) {
+            state.disableEncapsulation = true;
+          }
+        }
+      },
 
-            JSXOpeningElement: function transform(path, state) {
-                if (path.node.encapsulatedAlready || state.disableEncapsulation === true) {
-                    return;
-                }
+      JSXOpeningElement: function transform(path, state) {
+        if (
+          path.node.encapsulatedAlready ||
+          state.disableEncapsulation === true
+        ) {
+          return;
+        }
 
-                const ignoredElements = state.opts.ignoredElements || defaultIgnoredElements;
-                if (doesElementNameMatch(path.node.name, ignoredElements)) {
-                    return;
-                }
+        const ignoredElements =
+          state.opts.ignoredElements || defaultIgnoredElements;
+        if (doesElementNameMatch(path.node.name, ignoredElements)) {
+          return;
+        }
 
-              const className = makeClassNameFromPath(state.file.log.filename);
+        const className = makeClassNameFromPath(state.file.opts.filename);
 
-                const classnameAttributes = path.node.attributes
-                .filter(a=>t.isJSXAttribute(a) && t.isJSXIdentifier(a.name, {name: 'className'}));
-                if (!classnameAttributes.length) {
-                    const node = t.jSXOpeningElement(
-                        path.node.name,
-                        path.node.attributes.concat([t.jSXAttribute(
-                            t.jSXIdentifier('className'),
-                            t.stringLiteral(className)
-                        )]),
-                        path.node.selfClosing
-                    );
-                    node.encapsulatedAlready = true;
-                    path.replaceWith(node);
-                } else {
-                    classnameAttributes.forEach((attr)=>{
-                        if (t.isStringLiteral(attr.value)) {
-                            const node = t.jSXOpeningElement(
-                                path.node.name,
-                                path.node.attributes.map((curAttr)=>{
-                                    if (attr !== curAttr) return curAttr;
-                                    return t.jSXAttribute(
-                                        t.jSXIdentifier('className'),
-                                        t.stringLiteral(`${attr.value.value} ${className}`)
-                                    );
-                                }),
-                                path.node.selfClosing
-                            );
-                            node.encapsulatedAlready = true;
-                            path.replaceWith(node);
-                        } else if (t.isJSXExpressionContainer(attr.value)) {
-                            const node = t.jSXOpeningElement(
-                                path.node.name,
-                                path.node.attributes.map((curAttr)=>{
-                                    if (attr !== curAttr) return curAttr;
-                                    return t.jSXAttribute(
-                                        t.jSXIdentifier('className'),
-                                        t.jSXExpressionContainer(
-                                            t.binaryExpression(
-                                                '+',
-                                                attr.value.expression,
-                                                t.stringLiteral(` ${className}`)
-                                            )
-                                        )
-                                    );
-                                }),
-                                path.node.selfClosing
-                            );
-                            node.encapsulatedAlready = true;
-                            path.replaceWith(node);
-                        } else {
-                            throw new Error('Babel Plugin Encapsulate JSX: unknown attribute type');
-                        }
-                    });
-                }
-            },
-        },
-    };
+        const classnameAttributes = path.node.attributes.filter(
+          a =>
+            t.isJSXAttribute(a) &&
+            t.isJSXIdentifier(a.name, { name: "className" })
+        );
+        if (!classnameAttributes.length) {
+          const node = t.jSXOpeningElement(
+            path.node.name,
+            path.node.attributes.concat([
+              t.jSXAttribute(
+                t.jSXIdentifier("className"),
+                t.stringLiteral(className)
+              )
+            ]),
+            path.node.selfClosing
+          );
+          node.encapsulatedAlready = true;
+          path.replaceWith(node);
+        } else {
+          classnameAttributes.forEach(attr => {
+            if (t.isStringLiteral(attr.value)) {
+              const node = t.jSXOpeningElement(
+                path.node.name,
+                path.node.attributes.map(curAttr => {
+                  if (attr !== curAttr) return curAttr;
+                  return t.jSXAttribute(
+                    t.jSXIdentifier("className"),
+                    t.stringLiteral(`${attr.value.value} ${className}`)
+                  );
+                }),
+                path.node.selfClosing
+              );
+              node.encapsulatedAlready = true;
+              path.replaceWith(node);
+            } else if (t.isJSXExpressionContainer(attr.value)) {
+              const node = t.jSXOpeningElement(
+                path.node.name,
+                path.node.attributes.map(curAttr => {
+                  if (attr !== curAttr) return curAttr;
+                  return t.jSXAttribute(
+                    t.jSXIdentifier("className"),
+                    t.jSXExpressionContainer(
+                      t.binaryExpression(
+                        "+",
+                        attr.value.expression,
+                        t.stringLiteral(` ${className}`)
+                      )
+                    )
+                  );
+                }),
+                path.node.selfClosing
+              );
+              node.encapsulatedAlready = true;
+              path.replaceWith(node);
+            } else {
+              throw new Error(
+                "Babel Plugin Encapsulate JSX: unknown attribute type"
+              );
+            }
+          });
+        }
+      }
+    }
+  };
 };
